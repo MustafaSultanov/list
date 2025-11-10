e805de013486e453a34ecebacdc17c970fb3a282
Sultanov <mustafasultanov@Sultanovs-MacBook-Pro.local>
2025-11-11 00:16:14 +0600

    add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 				<Header />
 				{children}
+				<SpeedInsights />
 				<Footer />
 			</body>
 		</html>
